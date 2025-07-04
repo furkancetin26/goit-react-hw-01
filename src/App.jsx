add2294b3119d64ec41010eb31bffc5210a352c9
furkancetin <furkan130526@gmail.com>
2025-07-04 20:35:32 +0300

    last

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Profile from './components/Profile/Profile';
 import userData from './userData.json';
 import FriendList from './components/FriendList/FriendList';
 import friends from './friends.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './transactions.json';
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
       stats={userData.stats}
     />
     <FriendList friends={friends}/>
+    <TransactionHistory items={transactions} />
     </>
   )
 };
